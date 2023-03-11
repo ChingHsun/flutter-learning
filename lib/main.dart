5f2a8a7e59cb3de842dc32bcf8a48897e094ab44
Zoe Chen <carol331536@gmail.com>
2023-03-11 23:55:31 +0800

    feat: accessibility screen reader

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,12 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          // accessibility screen reader
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
